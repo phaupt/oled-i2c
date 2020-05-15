0313f89c8bba154ecf566cecffbe09974cbfed5c
phaupt <philipp.haupt@swisscom.com>
2020-05-15 11:56:21 +0200

    refactored

--- awakeOLED.sh
+++ awakeOLED.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-if ! (ps -ef | grep -q "[m]id.py") ; then (python /home/mid/oled-i2c/mid.py 30 &) ; else echo "already running..." ; fi
+if ! (ps -ef | grep -q "[m]id.py") ; then (python /home/mid/oled-i2c/mid.py 600 &) ; else echo "OLED is already on..." ; fi
