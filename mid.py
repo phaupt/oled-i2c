9f46f451775f0500049d14e0602088317d2ec563
phaupt <Philipp.Haupt@swisscom.com>
2020-06-06 16:59:11 +0200

    refactored

--- mid.py
+++ mid.py
@@ -78,7 +78,8 @@ while True:
     cmd = "/home/mid/oled-i2c/get-age.sh | tr -d '\n'"
     LastStkHeartbeat = subprocess.check_output(cmd, shell = True )
     
-    cmd = "awk '{printf(\"%02d:%02d\",($1/60/60%24),($1/60%60))}' /proc/uptime | tr -d '\n'"
+    #cmd = "awk '{printf(\"%02d:%02d\",($1/60/60%24),($1/60%60))}' /proc/uptime | tr -d '\n'"
+    cmd = "awk '{printf(\"%d\",($1/60/60/24))}' /proc/uptime | tr -d '\n'"
     Uptime = subprocess.check_output(cmd, shell = True )
     
     # now do the rest..
